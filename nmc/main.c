0bf903c97cae20e34391ed258517616da8810de9
Andrew Andrianov <andrew@ncrmnt.org>
2016-07-19 16:30:39 +0300

    First working cmake example
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- nmc/main.c
+++ nmc/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <easynmc/easynmc.h>
+//#include <easynmc/easynmc.h>
 
 void sleep(int k){
 	int f=3;
@@ -17,7 +17,7 @@ void sleep(int k){
 
 int main(int argc, char** argv)
 {
-	printf("Hello world from NeuroMatrix! I am the NMC printf'ing to you!\n");
+//	printf("Hello world from NeuroMatrix! I am the NMC printf'ing to you!\n");
 	return 0; 
 } 
 
